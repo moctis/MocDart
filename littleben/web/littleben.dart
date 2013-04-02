dee92113e2b945a7bfb1d3ca088cae59cdb02373
sitthidate nanuea <sitthidate@moctis.com>
2013-04-02 16:25:34 +0700

    +

--- littleben/web/littleben.dart
+++ littleben/web/littleben.dart
@@ -4,14 +4,15 @@ import 'package:web_ui/web_ui.dart';
 String currentTime;
 
 void main() {
-  DateTime today = new DateTime.now();
+  var today = new DateTime.now();
   currentTime = formatTime(today.hour, today.minute, today.second) ;
   currentTime = "[[{$currentTime}]]";
 }
 
 String formatTime(int h, int m, int s) {
   if (h > 12) { h = h - 12; }
-  String minute = (m <= 9) ? '0$m' : '$m';
-  String second = (s <= 9) ? '0$s' : '$s';
-  return '$h:$minute:$second';
+  var minute = (m <= 9) ? '0$m' : '$m';
+  var second = (s <= 9) ? '0$s' : '$s';
+
+  return '$h:$minute:$second'; 
 }
